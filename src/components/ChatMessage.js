9d3e5e9699844ba5584b793224487daa6a50a5ca
Brandon Roth <stew8908@gmail.com>
2021-03-17 20:55:28 -0500

    End of day 3

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -10,6 +10,7 @@ function ChatMessage() {
            <MessageContent>
            <Name>
                 James Boyd
+                <span>5/5/1986 11:35:55 AM</span>
            </Name>
             <Text>
                 This is cool
@@ -22,16 +23,36 @@ function ChatMessage() {
 export default ChatMessage
 
 const Container = styled.div`
-
+    padding: 8px 20px; 
+    display: flex;
+    align-items: center;
 `
 const UserAvatar = styled.div`
+    width: 36px;
+    height: 36px;
+    border-radius:2px;
+    overflow:hidden;
+    margin-right:8px;
+    img{
+        width:100%;
+    }
 `
 
 const MessageContent = styled.div`
+    display:flex;
+    flex-direction:column;
 `
-const Name = styled.span`
 
+const Name = styled.span`
+    font-weight:900;
+    font-size: 15px;
+    line-height: 1.4;
+    span{
+        margin-left: 8px;
+        font-weight: 400;
+        color: rgb(97, 96, 97);
+    }
 `
 const Text = styled.span`
-
+   
 `
